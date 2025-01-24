ee880d985dd24add1bc84b6bb227151a8269c8da
Ved Peesu <151007896+VedPeesu@users.noreply.github.com>
2025-01-24 12:06:43 -0800

    Create NewsAPIResponse.java

---
+++ NewsAPIResponse.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NewsApiResponse {
+    private List<Article> articles;
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Article {
+        private String title;
+        private String url;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+}
