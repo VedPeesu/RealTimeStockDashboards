3703f52e09f6a3a5899c1ce9c8b413307e0f2202
Ved Peesu <151007896+VedPeesu@users.noreply.github.com>
2025-01-24 12:06:03 -0800

    Update EmailSender.java

--- EmailSender.java
+++ EmailSender.java
@@ -2,40 +2,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EmailSenderService implements CommandLineRunner {
+@Service
+public class EmailSenderService {
+
     @Autowired
     private JavaMailSender mailSender;
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmailSenderService.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        if (args.length < 3) {
-            System.out.println("Usage: java EmailSenderService <toEmail> <subject> <body>");
-            return;
-        }
-
-        String toEmail = args[0];
-        String subject = args[1];
-        String body = args[2];
-
-        sendEmail(toEmail, subject, body);
-    }
-
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("");
+        message.setFrom("your_email@gmail.com");
         message.setTo(toEmail);
         message.setSubject(subject);
         message.setText(body);
 
         mailSender.send(message);
-        System.out.println("Mail Sent successfully...");
+        System.out.println("Email Sent successfully...");
     }
+}
